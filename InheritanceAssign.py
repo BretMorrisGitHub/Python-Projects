884d4e8429d8084d3f4aa826cc14eb0177d2de5a
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-05-16 10:59:13 -0700

    Update InheritanceAssign.py

--- InheritanceAssign.py
+++ InheritanceAssign.py
@@ -1,13 +1,16 @@
+#This class is the parent class defining the main properties
 class computer:
     CPU = ''
     RAM = ''
     Motherboard = ''
     Case = ''
 
+#This class is a child class of 'computer', with some unique properties
 class Desktop(computer):
     GPU = ''
     PowerSupply = ''
 
+#This class is also a child class of 'computer', with some unique properties
 class Laptop(computer):
     Battery = ''
     Screen = ''
